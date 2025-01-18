b3ee17e5e095e5b43ac53d1ae124ccc6cd8b9bbe
Raiyan7845 <raiyanmuhammed2003@gmail.com>
2025-01-18 14:40:37 +0530

    build

--- backend/config/conn.js
+++ backend/config/conn.js
@@ -1,18 +1,39 @@
+// var mysql = require('mysql');
+// var connection = mysql.createConnection({
+//   host: 'localhost',
+//   user: 'root',
+//   password: '',
+//   port: 3308,
+//   database: 'attendance_management'
+// });
+
+// connection.connect((err) => {
+//   if (err) {
+//       console.log('Error connecting to Db');
+//       return;
+//   }
+//   console.log('Connection established');
+// });
+
+// module.exports= connection;
+
 var mysql = require('mysql');
+require('dotenv').config();
+
 var connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  port: 3308,
-  database: 'attendance_management'
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+  database: process.env.DB_NAME,
 });
 
 connection.connect((err) => {
   if (err) {
-      console.log('Error connecting to Db');
-      return;
+    console.log('Error connecting to Db:', err.message);
+    return;
   }
   console.log('Connection established');
 });
 
-module.exports= connection;
+module.exports = connection;
